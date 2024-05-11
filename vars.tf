44fd89037e01094f061b06de9dcfc960f334e7c6
Boineni <78819238+Boineni@users.noreply.github.com>
2024-05-11 18:53:58 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    default = "ap-south-2"
+}
+
+variable "cluster_name" {
+    default = "nvvr-cluster"
+}
+
+variable "iam_role" {
+    default = "eks-role"
+}
+
+variable "vpc" {
+    default = "nvvr-vpc"
+}
